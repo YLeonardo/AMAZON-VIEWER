471ab3fa5487dc26de03898240bb86124fa53548
Leonardo Gutiérrez <leonardguti17@gmail.com>
2021-08-16 22:30:16 -0500

    Updated

---
+++ Amazon/src/amazon/Amazon.java
@@ -0,0 +1,43 @@
+package amazon;
+import java.util.Scanner;
+public class Amazon {
+
+   /* Amazon A = new Amazon();
+        System.out.println(A);
+
+   @Override
+    public String toString(){
+        return "Qué deseas ver en Amazon?";
+    }*/
+    public static int validateUserResponseMenu(int min, int max) {
+		//Leer la respuesta del usuario
+		Scanner sc = new Scanner(System.in);
+		
+		//Validar respuesta int
+		while(!sc.hasNextInt()) {
+			sc.next();
+			System.out.println("No ingresaste una opción válida1");
+			System.out.println("Intenta otra vez1");
+		}
+		
+		int response = sc.nextInt();
+		
+		//Validar rango de respuesta
+		while(response < min || response > max) {
+			//Solicitar de nuevo la respuesta
+			System.out.println("No ingresaste una opción válida");
+			System.out.println("Intenta otra vez");
+			
+			while(!sc.hasNextInt()) {
+				sc.next();
+				System.out.println("No ingresaste una opción válida");
+				System.out.println("Intenta otra vez");
+			}
+			response = sc.nextInt();
+		}
+		System.out.println("Tu Respuesta fue: " + response + "\n");
+		return response;
+	}
+
+    
+}
